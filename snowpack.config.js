e008ceab7033491a1504cd22ee9480dcfbd012cb
kiyotd <torigoedesign+kiyotd@gmail.com>
2021-01-27 11:49:34 +0900

    init

---
+++ snowpack.config.js
@@ -0,0 +1,24 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    // src: { url: "/", static: true }
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  exclude: [
+    "**/node_modules/**/*"
+  ]
+};
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./App.module.css"
+
+const App = () => {
+  return (
+    <div className={styles.main}>This is App Component.</div>
+  );
+};
+
+export default App;
